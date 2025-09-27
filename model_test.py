b09dd6d4ffb969cbee4fc0d5e181261990e79310
thunder848 <144604378+thunder848@users.noreply.github.com>
2025-09-27 12:49:49 +0800

    Update model_test.py

--- model_test.py
+++ model_test.py
@@ -32,7 +32,7 @@ def downsample_for_system(path_to_log,dic_traffic_env_conf):
 # TODO test on multiple intersections
 def test(model_dir, cnt_round, run_cnt, _dic_traffic_env_conf, if_gui):
     dic_traffic_env_conf = deepcopy(_dic_traffic_env_conf)
-    records_dir = model_dir.replace("model", "records")  # 将原来的model/改成records/
+    records_dir = model_dir.replace("model", "records") 
     model_round = "round_%d"%cnt_round
     dic_path = {}
     dic_path["PATH_TO_MODEL"] = model_dir
@@ -76,7 +76,7 @@ def test(model_dir, cnt_round, run_cnt, _dic_traffic_env_conf, if_gui):
                                                                dic_traffic_env_conf=dic_traffic_env_conf)
 
         done = False
-        state = env.reset()  # 得到36个路口的状态
+        state = env.reset()  
 
         for i in range(dic_traffic_env_conf['NUM_AGENTS']):
             agent_name = dic_exp_conf["MODEL_NAME"]
@@ -100,7 +100,7 @@ def test(model_dir, cnt_round, run_cnt, _dic_traffic_env_conf, if_gui):
             agents.append(agent)
 
             
-        # 将已经训练好的网络直接载入
+      
         for i in range(dic_traffic_env_conf['NUM_AGENTS']):
             if dic_traffic_env_conf["ONE_MODEL"]:
                 agents[i].load_network("{0}".format(model_round))
@@ -121,51 +121,13 @@ def test(model_dir, cnt_round, run_cnt, _dic_traffic_env_conf, if_gui):
 
                 if "CoLight" in dic_exp_conf["MODEL_NAME"]:
                     one_state = state  # dim(36,)
-                    action_list, attention = agents[i].choose_action(step_num, one_state)  # 36个路口根据自己的状态全部选择一个动作。
-                    cur_time = env.get_current_time()  # 此时虚拟环境的时间，会作为一个指标来进行评定
+                    action_list, attention = agents[i].choose_action(step_num, one_state) 
+                    cur_time = env.get_current_time()  
                     attention_dict[cur_time] = attention
                 elif "Hmasac" or "MASACGraph" or "MAPPOGraph" or "Hmappo" in dic_exp_conf["MODEL_NAME"]:
                     one_state = state  # dim(36,)
-#______________________________________________________________________________数据补齐
-                    # intersection_dic = {
-                    #     0: [1, 4],
-                    #     1: [0, 2, 5],
-                    #     2: [1, 3, 6],
-                    #     3: [2, 7],
-                    #     4: [0, 5, 8],
-                    #     5: [1, 4, 6, 9],
-                    #     6: [2, 5, 7, 10],
-                    #     7: [3, 6, 11],
-                    #     8: [4, 9, 12],
-                    #     9: [5, 8, 10, 13],
-                    #     10: [6, 9, 11, 14],
-                    #     11: [7, 10, 15],
-                    #     12: [8, 13],
-                    #     13: [9, 12, 14],
-                    #     14: [10, 13, 15],
-                    #     15: [11, 14],
-                    # }
-                    # # random_indices = torch.tensor([6,1,13,4,8,11,15])
-                    # random_indices = torch.tensor([6,1,13])
-                    # for node_idx in random_indices:
-                    #     # 获取该节点的邻居列表
-                    #     neighbors = intersection_dic.get(int(node_idx), [])
-                    #     neighbor_vehicle_counts = []
-                    #     for neighbor_idx in neighbors:
-                    #         # 确保邻居节点存在且有有效的lane_num_vehicle数据
-                    #         if 0 <= neighbor_idx < len(one_state):
-                    #             neighbor_data = one_state[neighbor_idx]
-                    #             if 'lane_num_vehicle' in neighbor_data:
-                    #                 neighbor_vehicle_counts.append(neighbor_data['lane_num_vehicle'])
-                    #     # 转换为NumPy数组
-                    #     arr = np.array(neighbor_vehicle_counts)
-                    #
-                    #     # 按列求和后取平均，并向上取整
-                    #     avg_vehicle_counts = np.ceil(np.mean(arr, axis=0)).astype(int).tolist()
-                    #     one_state[node_idx]['lane_num_vehicle'] = avg_vehicle_counts
-#______________________________________________________________________________________________________
-                    action_list = agents[i].choose_action(step_num, one_state)  # 36个路口根据自己的状态全部选择一个动作。
-                    cur_time = env.get_current_time()  # 此时虚拟环境的时间，会作为一个指标来进行评定
+                    action_list = agents[i].choose_action(step_num, one_state) 
+                    cur_time = env.get_current_time()  
                 elif "GCN" in dic_exp_conf["MODEL_NAME"]:
                     one_state = state
                     # print('one_state:',one_state)
